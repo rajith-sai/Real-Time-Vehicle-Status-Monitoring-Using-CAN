0a589c4a9537cffbbae917909e486a0a674a5f14
rajith-sai <151615410+rajith-sai@users.noreply.github.com>
2026-08-04 22:52:24 +0530

    Add interrupt definitions for external interrupts

---
+++ I2C/INTERRUPTS/interrupt_defines.h
@@ -0,0 +1,8 @@
+#define EINT0_0_1  0x0000000C      // Configure pin P0.1 to work as External Interrupt 0 (EINT0)
+#define EINT0_VIC_CHNO 14          // VIC (Vectored Interrupt Controller) channel number for EINT0
+
+#define EINT1_0_3  0x000000C0      // Configure pin P0.3 to work as External Interrupt 1 (EINT1)
+#define EINT1_VIC_CHNO 15          // VIC channel number assigned for EINT1
+
+#define EINT2_0_7  0x0000C000      // Configure pin P0.7 to work as External Interrupt 2 (EINT2)
+#define EINT2_VIC_CHNO 16          // VIC channel number assigned for EINT2
